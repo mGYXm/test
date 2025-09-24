4a3b7af122e61871aeb5854ce2bb2315e6936a18
Милов Максим Олегович <m.milov@univ.priv>
2025-09-24 13:08:22 +0300

    change output, add 2

--- Hello.java
+++ Hello.java
@@ -1,8 +1,7 @@
 
 public class Hello{
     public static void main(String[] args) {
+        System.out.println("Hello, world!2");
 
-        System.out.println("Hello world");
-        System.out.println("B");
     }
 }
